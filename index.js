590781aebe6337aaabe0363dd0390e935c129d91
rivendo21 <abdelrahman2729@gmail.com>
2026-01-12 20:35:16 +0200

    fixing error in displaying correct time

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function updateClock() {
   let now = new Date();
-
-  let hours = now.getHours().toString().padStart(2, 0);
+  let hours = now.getHours();
+  let stdTime = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12;
+  hours = hours.toString().padStart(2, 0);
   let minutes = now.getMinutes().toString().padStart(2, 0);
   let seconds = now.getSeconds().toString().padStart(2, 0);
-  let stdTime = hours >= 12 ? "PM" : "AM";
-  hours = Math.floor(hours / 2) - 1;
   document.getElementById(
     "clock"
-  ).innerHTML = `${hours}:${minutes}:${seconds}${stdTime}`;
+  ).textContent = `${hours}:${minutes}:${seconds}${stdTime}`;
 }
 updateClock();
-setInterval(updateClock);
+setInterval(updateClock, 1000);
